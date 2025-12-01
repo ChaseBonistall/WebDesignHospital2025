432a6b992f79579f980d7dabb87bc8cad09551d4
ChaseBonistall <cb43796@georgiasouthern.edu>
2025-12-01 16:52:34 -0500

    hospital database

---
+++ hospital_info.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `user_info` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+)
