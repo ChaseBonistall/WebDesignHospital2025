1c68c90df1dd73a63fbcc8245b13d541ed56b65f
ChaseBonistall <cb43796@georgiasouthern.edu>
2025-12-03 15:36:38 -0500

    Update hospital_info.sql

--- hospital_info.sql
+++ hospital_info.sql
@@ -5,3 +5,23 @@ CREATE TABLE `user_info` (
   `password` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 )
+
+INSERT INTO `doctors` (`doctor_id`, `name`) VALUES
+(1, 'Dr. Sarah Leslen'),
+(2, 'Dr. Bridget Axle'),
+(3, 'Dr. Emily Stanford'),
+(4, 'Dr. Danny DeCheeto'),
+(5, 'Dr. Michael Arcen'),
+(6, 'Dr. Robert Jones'),
+(7, 'Dr. Anne Frank'),
+(8, 'Dr. David Kim'),
+(9, 'Dr. Jennifer Brown'),
+(10, 'Dr. Ben Dover');
+
+CREATE TABLE `scheduled_appt` (
+  `appointment_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `doctor_id` int(11) NOT NULL,
+  `appointment_date` date NOT NULL,
+  `appointment_time` time NOT NULL
+)
