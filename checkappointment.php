fd110713b05b3249001c554e6c826e6edd363e2c
ChaseBonistall <cb43796@georgiasouthern.edu>
2025-12-03 20:32:01 -0500

    Update checkappointment.php

--- checkappointment.php
+++ checkappointment.php
@@ -2,23 +2,20 @@
 session_start();
 require 'db_connection.php';
 
-// User must be logged in
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit;
 }
 
 $user_id = $_SESSION['user_id'];
-
-// Correct SQL for YOUR tables
 $sql = "
-SELECT s.appointment_id, s.appointment_date, s.appointment_time,
-       d.name AS doctor_name
-FROM scheduled_appt s
-JOIN doctors d ON s.doctor_id = d.doctor_id
-WHERE s.user_id = ?
-ORDER BY s.appointment_date DESC, s.appointment_time DESC
-";
+    SELECT s.appointment_id, s.appointment_date, s.appointment_time,
+    d.name AS doctor_name
+    FROM scheduled_appt s
+    JOIN doctors d ON s.doctor_id = d.doctor_id
+    WHERE s.user_id = ?
+    ORDER BY s.appointment_date DESC, s.appointment_time DESC
+    ";
 
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $user_id);
@@ -49,19 +46,19 @@ $result = $stmt->get_result();
             <th>Actions</th>
         </tr>
 
-        <?php while ($row = $result->fetch_assoc()): ?>
-        <tr>
-            <td><?php echo $row['appointment_id']; ?></td>
-            <td><?php echo $row['doctor_name']; ?></td>
-            <td><?php echo $row['appointment_date']; ?></td>
-            <td><?php echo $row['appointment_time']; ?></td>
-            <td>
-                <a href="editappointment.php?id=<?php echo $row['appointment_id']; ?>">Edit</a> |
-                <a href="deleteappointment.php?id=<?php echo $row['appointment_id']; ?>"
-                   onclick="return confirm('Are you sure you want to delete this appointment?');">
-                   Delete
-                </a>
-            </td>
+    <?php while ($row = $result->fetch_assoc()): ?>
+    <tr>
+        <td><?php echo $row['appointment_id']; ?></td>
+        <td><?php echo $row['doctor_name']; ?></td>
+        <td><?php echo $row['appointment_date']; ?></td>
+        <td><?php echo $row['appointment_time']; ?></td>
+        <td>
+        <a href="editappointment.php?id=<?php echo $row['appointment_id']; ?>">Edit</a> |
+            <a href="deleteappointment.php?id=<?php echo $row['appointment_id']; ?>"
+                onclick="return confirm('Are you sure you want to delete this appointment?');">
+                Delete
+            </a>
+        </td>
         </tr>
         <?php endwhile; ?>
     </table>
